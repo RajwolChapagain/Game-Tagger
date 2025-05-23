4e59c7391c9f950494d7e1b836c57da769c645da
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-23 14:53:12 -0400

    Add torch and numpy

--- flake.nix
+++ flake.nix
@@ -5,15 +5,31 @@
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 	};
 
+	nixConfig = {
+		substituters = [
+				"https://nix-community.cachix.org"
+		];
+		trusted-public-keys = [
+				"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+		];
+	};
+
 	outputs = { self, nixpkgs }: 
 	let
-		pkgs = nixpkgs.legacyPackages.x86_64-linux;
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { 
+			system = system; 
+			config.allowUnfree = true;
+
+		};
 	in
 	{
 		devShells.x86_64-linux.default = pkgs.mkShell rec {
 			python-packages = with pkgs.python312Packages; [
 				requests
 				beautifulsoup4
+				numpy
+				torchWithCuda
 			];
 
 			packages = with pkgs; [
