2b21b3688ce5bb9b1bd1002659b6f9c2df97b4a3
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-25 12:48:20 -0400

    Make split.py work with new data directory structure

--- split.py
+++ split.py
@@ -1,19 +1,18 @@
 import os
-import argparse
 import random
 from pathlib import Path
 from multiprocessing import Pool
 
-def split_dir(dir: str):
-    shuffled_image_files = os.listdir(dir)
+def split_dir(dir: str, data_dir: Path = Path('./data')):
+    shuffled_image_files = os.listdir(f'{data_dir}/{dir}')
     random.shuffle(shuffled_image_files)
     num_images = len(shuffled_image_files)
     num_train = int(split_ratio * num_images)
     train_image_files = shuffled_image_files[:num_train]
     test_image_files = shuffled_image_files[num_train:]
 
-    split_train_dir = Path(f'./train/{dir}')
-    split_test_dir = Path(f'./test/{dir}')
+    split_train_dir = data_dir / 'train' / dir
+    split_test_dir = data_dir / 'test' / dir
 
     if not split_train_dir.exists():
         os.mkdir(split_train_dir)
@@ -22,19 +21,20 @@ def split_dir(dir: str):
         os.mkdir(split_test_dir)
 
     for file in train_image_files:
-        os.system(f'cp {dir}/{file} {split_train_dir}')
+        os.system(f'cp {data_dir}/{dir}/{file} {split_train_dir}')
 
     for file in test_image_files:
-        os.system(f'cp {dir}/{file} {split_test_dir}')
+        os.system(f'cp {data_dir}/{dir}/{file} {split_test_dir}')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dirs', nargs='+', help='List of directories to split')
+    data_dir = Path('./data')
 
-    args = parser.parse_args()
+    if not data_dir.exists():
+        print(f'Error: {data_dir} directory doesn\'t exist in the current directory')
+        exit()
 
-    train_dir = Path('./train')
-    test_dir = Path('./test')
+    train_dir = data_dir / 'train'
+    test_dir = data_dir / 'test'
 
     if not train_dir.exists():
         os.mkdir(train_dir)
@@ -44,7 +44,9 @@ if __name__ == '__main__':
     
     split_ratio = 0.75
 
-    valid_dirs = [ dir for dir in args.dirs if dir != train_dir and dir != test_dir and os.path.isdir(dir) ]
+    all_dirs = os.listdir(data_dir)
+
+    valid_dirs = [ dir for dir in all_dirs if (data_dir / dir) != train_dir and (data_dir / dir) != test_dir ]
 
     with Pool(len(valid_dirs)) as p:
         p.map(split_dir, valid_dirs)
