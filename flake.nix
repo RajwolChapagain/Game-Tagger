14229b10fe2d8abdbbefd573b331d744f9bb9702
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-05-26 11:37:05 -0400

    Include nix cache and add matplotlib and torchvision

--- flake.nix
+++ flake.nix
@@ -8,9 +8,11 @@
 	nixConfig = {
 		substituters = [
 				"https://nix-community.cachix.org"
+				"https://cache.nixos.org/"
 		];
 		trusted-public-keys = [
 				"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+				"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 		];
 	};
 
@@ -30,6 +32,8 @@
 				beautifulsoup4
 				numpy
 				torchWithCuda
+				matplotlib
+				torchvision
 			];
 
 			packages = with pkgs; [
